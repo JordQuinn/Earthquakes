1624ff472e81ac4e217f3cbd7f398f8326ec7629
Your Name <jordanmichelequinn@gmail.com>
2017-09-01 09:33:06 -0700

    finished styling to match the mockup

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-
-//import EarthquakeInfo from '../components/EarthquakeInfo.js';
+import EarthquakeInfo from '../components/EarthquakeInfo.js';
 import  '../styles/App.css';
+import moment from "moment";
+import EarthquakeList from '../components/EarthquakeList';
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
+        <div>
+            <div className="my-header">Earthquakes!</div>
         <div className="title">
-          <div className="my-header">Earthquakes!
+          <EarthquakeInfo/>
+          <EarthquakeList/>
+
           </div>
         </div>
       </div>
--- src/components/EarthquakeInfo.js
+++ src/components/EarthquakeInfo.js
@@ -4,7 +4,11 @@ import moment from 'moment';
 export default class EarthquakeInfo extends Component {
   render() {
     return (
-      <div className = "earthquake-title"> This is a list earthquakes occurring on {moment().format("MMM Do")} across the United States.</div>
+      <div className = "earthquake-title"> This is a list earthquakes occurring on July 14th, 2017 across the United States.</div>
     )
   }
 }
+
+
+
+// or use this to get current date for hard mode: {moment().format("MMM Do")} 
